fc4176ad2461fce94d2e8144adc528b860c01c5e
evog-id <evgenijsvog@gmail.com>
2025-03-06 07:09:52 +0200

    Update header.php

--- oc-content/themes/delta/header.php
+++ oc-content/themes/delta/header.php
@@ -67,21 +67,6 @@
           </span>
         </a>
 
-    <div class="relative3">
-     <div class="home-container slider">
-      <div class="inner">
-        <div id="home-slider">
-           <div id="sliders">
-              <div class="rslides_container">
-                   <ul id="slider" class="rslides">
-                      <li><a href="www.e-tirgus.eu/"> <img src="https://market.asnocode.com/oc-content/themes/delta/images/slide4.jpg" /> </a></li>
-                    </ul>
-              </div>
-          </div>
-         </div>
-       </div>
-     </div> 
-   </div>
         
         <?php if(!osc_is_web_user_logged_in()) { ?>
           <a class="register" href="<?php echo osc_register_account_url(); ?>"><?php _e('Register', 'delta'); ?></a>
@@ -132,7 +117,24 @@
         <?php } ?>
       </div>
     </div>
-    
+
+     <div class="relative3">
+     <div class="home-container slider">
+      <div class="inner">
+        <div id="home-slider">
+           <div id="sliders">
+              <div class="rslides_container">
+                   <ul id="slider" class="rslides">
+                      <li><a href="www.e-tirgus.eu/"> <img src="https://market.asnocode.com/oc-content/themes/delta/images/slide4.jpg" /> </a></li>
+                    </ul>
+              </div>
+          </div>
+         </div>
+       </div>
+     </div> 
+   </div>
+
+              
     <?php osc_run_hook('header_bottom'); ?>
   </div>
 </header>
