d61546a7df4d793f1e742344c48a5437bd060f19
evog-id <evgenijsvog@gmail.com>
2025-03-25 20:59:29 +0200

    Update set_storage.php

--- set_storage.php
+++ set_storage.php
@@ -7,6 +7,15 @@ use Utopia\Storage\Device\Local;
 use Utopia\Storage\Device\S3;
 use Utopia\Storage\Device\DOSpaces;
 
+// Configuration
+$SPACES_BUCKET_NAME = getenv('SPACES_BUCKET_NAME');
+$SPACES_REGION = getenv('SPACES_REGION');
+$SPACES_ACCESS_KEY = getenv('SPACES_ACCESS_KEY');
+$SPACES_SECRET_KEY = getenv('SPACES_SECRET_KEY');
+$SPACES_ENDPOINT = getenv('SPACES_ENDPOINT');
+$AWS_ACL_FLAG = 'ACL_PUBLIC'
+
+
 // Instantiating local storage
 // Storage::setDevice('files', new Local('path'));
 
@@ -14,7 +23,7 @@ use Utopia\Storage\Device\DOSpaces;
 // Storage::setDevice('files', new S3('path', AWS_ACCESS_KEY, AWS_SECRET_KEY,AWS_BUCKET_NAME, AWS_REGION, AWS_ACL_FLAG));
 
 // Or you can use DigitalOcean Spaces storage
-Storage::setDevice('files', new DOSpaces('path', SPACES_ACCESS_KEY, SPACES_SECRET_KEY, SPACES_BUCKET_NAME, SPACES_REGION, AWS_ACL_FLAG));
+Storage::setDevice('files', new DOSpaces('path', $SPACES_ACCESS_KEY, $SPACES_SECRET_KEY, $SPACES_BUCKET_NAME, $SPACES_REGION, $AWS_ACL_FLAG));
 
 $device = Storage::getDevice('files');
 
